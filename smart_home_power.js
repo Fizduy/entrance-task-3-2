0c40ae82edfb5873ba0ee7fd53f075ef20d0fa33
veryhardstuff <veryhardstuff@gmail.com>
2018-08-31 11:46:42 -0400

    fix schedule in mode-devices

--- smart_home_power.js
+++ smart_home_power.js
@@ -4,17 +4,8 @@ export function home_schedule (input_data){
     /*Объект расписания с исходными данными */
     const schedule = [];
   
-    function compare(i,from,to, callback){
-      if(i >= to){
-        if(to < from && from <= i){
-          if(i > 23) callback();  
-        }else return false;
-       }
-        return true;
-     }
-  
     input_data.rates.forEach(rate => {
-        for(let i = rate.from; compare(i,rate.from,rate.to,()=>{i = 0}); ++i){
+        for(let i = rate.from; hour_compare(i,rate.from,rate.to,()=>{i = 0}); ++i){
             schedule[i] = {'rate':rate.value};
             schedule[i].maxPower = input_data.maxPower;
             schedule[i].devices = [];
@@ -24,14 +15,20 @@ export function home_schedule (input_data){
     const mode = {
         undefined : {
             "duration": 24,
+            "from": 0,
+            "to": 23,
             "total_power":input_data.maxPower*24
         },
         "day": {
             "duration": 14,
+            "from": 7,
+            "to": 21,
             "total_power":input_data.maxPower*14
         },
         "night": {
             "duration": 10,
+            "from": 21,
+            "to": 7,
             "total_power": input_data.maxPower*14
         }
     }; /* todo: реализовать total_power с использованием this.duration */
@@ -49,7 +46,7 @@ export function home_schedule (input_data){
     input_data.devices.forEach(device => {
         device.start_at = false;
 
-        /*Тест пика мощности - todo: вынести из цикла, либо реализовать тест для пересечений приборов менее 24 часа*/
+        /*Тест пика мощности - todo: вынести из цикла, либо реализовать тест для пересечений с приборами менее 24 часа*/
         let device_maxPower = input_data.maxPower - device.power;
         for(let i=0;devices_duration[i].duration > 23; ++i){
             if(devices_duration[i].id != device.id){
@@ -88,11 +85,17 @@ export function home_schedule (input_data){
 
         /*Расчет стоимости включений прибора, для 24-приборов только один расчет, от 0:00*/
         device.schedule = [];
-        for (let [i,end] = [0, (device.duration < 24) ? 24 : 1]; i < end; ++i){
-          device.schedule[i] = {'price' : device.power*schedule[i].rate, 'start' : i};
-          for(let [d,s] = [1,0]; d < device.duration; ++d){
+        
+        let to = (device.mode === undefined) ? mode[device.mode].to : mode[device.mode].to - device.duration;
+        if(to < 0){
+          to (device.duration == 24) ? 1 : to + 24;
+        }
+        
+        for (let i = mode[device.mode].from;hour_compare(i,mode[device.mode].from,to,()=>{i = 0}); ++i){
+          device.schedule.push({'price' : device.power*schedule[i].rate, 'start' : i});
+          for(let [d,l,s] = [1,device.schedule.length-1,0]; d < device.duration; ++d){
             s = (i+d > 23) ? i+d-24 : i+d;
-            device.schedule[i].price += device.power*schedule[s].rate;
+            device.schedule[l].price += device.power*schedule[s].rate;
           }
         }
       
@@ -102,59 +105,36 @@ export function home_schedule (input_data){
                 schedule[i].maxPower -= device.power;
                 schedule[i].devices.push(device.id);
             }
-            device.start = 0;
+            device.start_at = 0;
+            device.price_delta = 0;
+            device.price_min_delta = 0;
         }else{
-          /* Сортировака п опотреблению */
+          /* Сортировака остальных по потреблению */
           device.schedule.sort((a,b)=>{return a.price - b.price;})
-          device.price_delta = device.schedule[23].price-device.schedule[0].price;
+          device.price_delta = device.schedule[device.schedule.length-1].price - device.schedule[0].price;
+          
+          let min_delta = 1;
+          for(;device.schedule[min_delta].price === device.schedule[0].price;++min_delta);
+          device.price_min_delta = device.schedule[min_delta].price - device.schedule[0].price;
         }
 
     });
     
-    /* todo: расстановка остальныз приборов + перестоновка с рекурсией */
-    const devices_delta = input_data.devices.slice().sort((a,b)=>{return b.price_delta - a.price_delta;});
+    /* todo: расстановка остальных приборов + перестоновка с рекурсией */
+    const devices_delta = input_data.devices.slice().sort((a,b)=>{return b.price_min_delta - a.price_min_delta;});
     devices_delta.forEach(device => {
        
      });
 
-    const test_data = {
-        "schedule": {
-            "0": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E", "F972B82BA56A70CC579945773B6866FB"],
-            "1": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E", "F972B82BA56A70CC579945773B6866FB"],
-            "2": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E", "F972B82BA56A70CC579945773B6866FB"],
-            "3": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E"],
-            "4": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E"],
-            "5": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E"],
-            "6": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E"],
-            "7": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E"],
-            "8": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E"],
-            "9": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E"],
-            "10": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E", "C515D887EDBBE669B2FDAC62F571E9E9"],
-            "11": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E", "C515D887EDBBE669B2FDAC62F571E9E9"],
-            "12": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E"],
-            "13": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E"],
-            "14": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E"],
-            "15": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E"],
-            "16": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E"],
-            "17": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E"],
-            "18": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E"],
-            "19": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E"],
-            "20": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E"],
-            "21": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E"],
-            "22": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E"],
-            "23": ["02DDD23A85DADDD71198305330CC386D", "1E6276CC231716FE8EE8BC908486D41E", "7D9DC84AD110500D284B33C82FE6E85E"]
-        },
-        "consumedEnergy": {
-            "value": 38.939,
-            "devices": {
-                "F972B82BA56A70CC579945773B6866FB": 5.1015,
-                "C515D887EDBBE669B2FDAC62F571E9E9": 21.52,
-                "02DDD23A85DADDD71198305330CC386D": 5.398,
-                "1E6276CC231716FE8EE8BC908486D41E": 5.398,
-                "7D9DC84AD110500D284B33C82FE6E85E": 1.5215
-            }
-        }
-      };
 
-return test_data;
+return 'data is`t ready!';
 }
+
+function hour_compare(i,from,to, callback){
+      if(i >= to){
+        if(to < from && from <= i){
+          if(i > 23) callback();  
+        }else return false;
+       }
+        return true;
+     }
