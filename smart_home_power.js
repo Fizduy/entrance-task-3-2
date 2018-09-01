8ae92087e9f4ece56316c77375fde4d4b1a46552
Fizduy <34884945+Fizduy@users.noreply.github.com>
2018-09-01 16:33:19 +0300

    refact schedule/avalibale

--- smart_home_power.js
+++ smart_home_power.js
@@ -25,8 +25,11 @@ export function home_schedule(input_data) {
         m.total_power = m.duration * input_data.maxPower;
     });
 
-    /*Объект расписания с исходными данными */
-    let schedule = schedule_construct(input_data.rates, input_data.maxPower);
+    let schedule = {};
+    for(let i = 0; i <24 ; schedule[i++]=[]);
+
+    /*Объект расписания с доступной мощностью и ценой*/
+    let available = available_construct(input_data.rates, input_data.maxPower);
 
     /*Тест превышения прибором максимальной мощности и сортировка*/
     input_data.devices.sort((a, b) => { return b.power - a.power; });
@@ -48,23 +51,23 @@ export function home_schedule(input_data) {
         total_power_counter(device, mode)
 
         /*Расчет стоимости включений прибора, для 24-приборов только один расчет, от 0:00*/
-        device.schedule = device_schedule_construct(device, schedule, mode);
+        device.available = device_available_construct(device, available, mode);
 
         /* Расстановка приборов 24 */
         if (device.duration == 24) {
             for (let t = 0; t < device.duration; ++t) {
-                schedule[t].maxPower -= device.power;
-                schedule[t].devices.push(device.id);
+                available[t].maxPower -= device.power;
+                schedule[t].push(device.id);
             }
-            Object.assign(device,{start_at:0,price_delta:0,price_min_delta:0});
+            Object.assign(device,{start_at:0,price_delta:0,price_min_delta:0,schedule_price:device.available[0].price});
         } else {
             /* Сортировака остальных по потреблению */
-            device.schedule.sort((a, b) => { return a.price - b.price; })
-            device.price_delta = device.schedule[device.schedule.length - 1].price - device.schedule[0].price;
+            device.available.sort((a, b) => { return a.price - b.price; })
+            device.price_delta = device.available[device.available.length - 1].price - device.available[0].price;
 
             let min_delta = 1;
-            for (; device.schedule[min_delta].price === device.schedule[0].price; ++min_delta);
-            device.price_min_delta = device.schedule[min_delta].price - device.schedule[0].price;
+            for (; device.available[min_delta].price === device.available[0].price; ++min_delta);
+            device.price_min_delta = device.available[min_delta].price - device.available[0].price;
         }
 
     });
@@ -72,7 +75,7 @@ export function home_schedule(input_data) {
     /* Расстановка остальных приборов. todo: перестоновка с рекурсией */
     const devices_delta = input_data.devices.slice().sort((a, b) => { return b.price_min_delta - a.price_min_delta; });
 
-    devices_delta.forEach(device => put_device_in_schedule(device, schedule, new_schedule => {schedule = new_schedule}));
+    devices_delta.forEach(device => put_device_in_schedule(device,schedule,available, new_data => {[schedule,available] = new_data;}));
 
     return schedule;
 }
@@ -86,18 +89,18 @@ function hour_counter(i, from, to, callback) {
     return true;
 }
 
-function schedule_construct (rates,maxPower){
-    const schedule = []; 
+function available_construct (rates,maxPower){
+    const available = []; 
     rates.forEach(rate => {
         for(let i = rate.from; hour_counter(i,rate.from,rate.to,()=>{i = 0}); ++i){
-            schedule[i] = {
+            available[i] = {
                 rate:rate.value,
                 maxPower:maxPower,
                 devices:[]
             };
         }
     });
-    return schedule;
+    return available;
 }
 
 /*todo: power_peak_test требует доработки учета пересечений с не клуглосуточными приборами */
@@ -140,49 +143,51 @@ function total_power_counter(device,mode) {
     });
 }
 
-function device_schedule_construct(device, schedule, mode) {
-    let dev_schedule = [];
+function device_available_construct(device, available, mode) {
+    let dev_available = [];
     let to = (device.mode === undefined) ? mode[device.mode].to : mode[device.mode].to - device.duration;
     if (to < 0) {
         to = (device.duration == 24) ? 1 : to + 24;
     }
 
     for (let i = mode[device.mode].from; hour_counter(i, mode[device.mode].from, to, () => { i = 0 }); ++i) {
-        dev_schedule.push({ price: device.power/1000 * schedule[i].rate, 'start': i });
+        dev_available.push({ price: device.power/1000 * available[i].rate, 'start': i });
         let end = 0;
-        let dev_i = dev_schedule.length - 1;
+        let dev_i = dev_available.length - 1;
         for (let d = 1; d < device.duration; ++d) {
             end = (i + d > 23) ? i + d - 24 : i + d;
-            dev_schedule[dev_i].price += device.power/1000 * schedule[end].rate;
+            dev_available[dev_i].price += device.power/1000 * available[end].rate;
         }
-        dev_schedule[dev_i].stop = end + 1;
+        dev_available[dev_i].stop = end + 1;
     }
-    return dev_schedule;
+    return dev_available;
 }
 
-function put_device_in_schedule(device, schedule, callback) {
+function put_device_in_schedule(device,schedule,available, callback) {
     /*Обход всех доступных расписаний*/
-    for (let s = 0; s < device.schedule.length; ++s) {
+    for (let s = 0; s < device.available.length; ++s) {
         let checkout = true;
-        const schedule_temp = Object.assign([], schedule);
+        const available_temp = Object.assign([], available);
+        const schedule_temp = Object.assign({}, schedule);
         /*Проверка доступной мощности и предварительное заполнение расписания*/
-        for (let t = device.schedule[s].start; hour_counter(t, device.schedule[s].start, device.schedule[s].stop, () => { t = 0 }); ++t) {
-            if (schedule[t].maxPower >= device.power) {
-                schedule_temp[t] = {
-                    rate: schedule[t].rate,
-                    maxPower: schedule[t].maxPower - device.power,
-                    devices: schedule[t].devices.slice()
+        for (let t = device.available[s].start; hour_counter(t, device.available[s].start, device.available[s].stop, () => { t = 0 }); ++t) {
+            if (available[t].maxPower >= device.power) {
+                available_temp[t] = {
+                    rate: available[t].rate,
+                    maxPower: available[t].maxPower - device.power
                 };
-                schedule_temp[t].devices.push(device.id);
+                schedule_temp[t] = schedule_temp[t].slice();
+                schedule_temp[t].push(device.id);
             } else {
                 checkout = false;
                 break;
             }
         }
         if (checkout) {
-            device.start_at = device.schedule[s].start;
-            callback(schedule_temp);
-            return schedule_temp;
+            device.schedule_price = device.available[s].price;
+            device.start_at = device.available[s].start;
+            callback([schedule_temp,available_temp]);
+            return [schedule_temp,available_temp];
         }
     }
     throw Error(`Can't put id:${device.id} to schedule!`);
